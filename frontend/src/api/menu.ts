c0ecffafe6d61cc0718286e4dae1e3779e7d24e1
ssondh <blue951208@naver.com>
2026-04-23 16:45:26 +0900

    요청 경로 구분

--- frontend/src/api/menu.ts
+++ frontend/src/api/menu.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const isProd = import.meta.env.PROD;
+axios.defaults.baseURL = isProd
+    ? 'https://handyvue.onrender.com/'
+    : 'http://localhost:8080';
+
 export const getMenuList = () => {
     console.log('getMenuList');
-    return axios.get('http://localhost:8080/api/cmMenuMst/list');
+    return axios.get('/api/cmMenuMst/list');
 }
